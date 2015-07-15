0933036e91096127a1286acb1a07e48be17a11aa
s.matyukevich <s.matyukevich@gmail.com>
2015-07-15 16:06:08 +0300

    modified

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -16,104 +16,6 @@ output "aws_vpc_id" {
 	value = "${aws_vpc.default.id}"
 }
 
-resource "aws_internet_gateway" "default" {
-	vpc_id = "${aws_vpc.default.id}"
-}
-
-output "aws_internet_gateway_id" {
-	value = "${aws_internet_gateway.default.id}"
-}
-
-
-# NAT instance
-
-resource "aws_security_group" "nat" {
-	name = "nat"
-	description = "Allow services from the private subnet through NAT"
-	vpc_id = "${aws_vpc.default.id}"
-
-	ingress {
-		from_port = 22
-		to_port = 22
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 80
-		to_port = 80
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 123
-		to_port = 123
-		protocol = "udp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 443
-		to_port = 443
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 4443
-		to_port = 4443
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		from_port = 9418
-		to_port = 9418
-		protocol = "tcp"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	ingress {
-		cidr_blocks = ["0.0.0.0/0"]
-		from_port = -1
-		to_port = -1
-		protocol = "icmp"
-	}
-
-	egress {
-		from_port = 0
-		to_port = 0
-		protocol = "-1"
-		cidr_blocks = ["0.0.0.0/0"]
-	}
-
-	tags {
-		Name = "${var.aws_vpc_name}-nat"
-	}
-
-}
-
-resource "aws_instance" "nat" {
-	ami = "${lookup(var.aws_nat_ami, var.aws_region)}"
-	instance_type = "t2.small"
-	key_name = "${var.aws_key_name}"
-	security_groups = ["${aws_security_group.nat.id}"]
-	subnet_id = "${aws_subnet.bastion.id}"
-	associate_public_ip_address = true
-	source_dest_check = false
-	tags {
-		Name = "nat"
-	}
-}
-
-resource "aws_eip" "nat" {
-	instance = "${aws_instance.nat.id}"
-	vpc = true
-}
-
-# Public subnets
-
 resource "aws_subnet" "bastion" {
 	vpc_id = "${aws_vpc.default.id}"
 	cidr_block = "${var.network}.0.0/24"
@@ -126,36 +28,9 @@ output "bastion_subnet" {
 	value = "${aws_subnet.bastion.id}"
 }
 
-output "aws_subnet_bastion_availability_zone" {
-	value = "${aws_subnet.bastion.availability_zone}"
-}
-
-# Routing table for public subnets
-
-resource "aws_route_table" "public" {
-	vpc_id = "${aws_vpc.default.id}"
-
-	route {
-		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_internet_gateway.default.id}"
-	}
-}
-
-output "aws_route_table_public_id" {
-	value = "${aws_route_table.public.id}"
-}
-
-resource "aws_route_table_association" "bastion-public" {
-	subnet_id = "${aws_subnet.bastion.id}"
-	route_table_id = "${aws_route_table.public.id}"
-}
-
-# Private subsets
-
 resource "aws_subnet" "microbosh" {
 	vpc_id = "${aws_vpc.default.id}"
 	cidr_block = "${var.network}.1.0/24"
-	availability_zone = "${aws_subnet.bastion.availability_zone}"
 	tags {
 		Name = "${var.aws_vpc_name}-microbosh"
 	}
@@ -163,26 +38,6 @@ resource "aws_subnet" "microbosh" {
 
 output "aws_subnet_microbosh_id" {
   value = "${aws_subnet.microbosh.id}"
-}
-
-# Routing table for private subnets
-
-resource "aws_route_table" "private" {
-	vpc_id = "${aws_vpc.default.id}"
-
-	route {
-		cidr_block = "0.0.0.0/0"
-		instance_id = "${aws_instance.nat.id}"
-	}
-}
-
-output "aws_route_table_private_id" {
-	value = "${aws_route_table.private.id}"
-}
-
-resource "aws_route_table_association" "microbosh-private" {
-	subnet_id = "${aws_subnet.microbosh.id}"
-	route_table_id = "${aws_route_table.private.id}"
 }
 
 resource "aws_security_group" "bastion" {
@@ -235,6 +90,3 @@ output "aws_security_group_bastion_id" {
   value = "${aws_security_group.bastion.id}"
 }
 
-output "aws_security_group_nat_id" {
-  value = "${aws_security_group.nat.id}"
-}
